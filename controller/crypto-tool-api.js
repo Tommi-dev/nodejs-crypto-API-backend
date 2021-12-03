48ce6bee8fc2b3fcd1190ff3ea3bf33f350c6f03
Tommi <tmmi.makela@outlook.com>
2021-12-03 23:29:58 +0200

    refactoring

--- controller/crypto-tool-api.js
+++ controller/crypto-tool-api.js
@@ -1,42 +1,5 @@
 const logger = require('../utils/logger')
-const { errorHandler } = require('../utils/middleware')
-
-const returnRequestBody = async (request) => {
-
-  let body = await new Promise(function (resolve, reject) {
-
-    let requestBody = []
-    request.on('data', (chunk) => {
-      requestBody.push(chunk)
-    })
-
-    request.on('end', () => {
-      try {
-        requestBody = JSON.parse(Buffer.concat(requestBody))
-      } catch (err) {
-        reject(err)
-      }
-      resolve(requestBody)
-    })
-  })
-
-  return body
-}
-
-const sendResponseBodyToClient = (body, response) => {
-
-  body = JSON.stringify(body)
-
-  const headers = {
-    'Content-Type': 'application/json;charset=utf-8',
-    'Content-Length': Buffer.byteLength(body),
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'POST'
-  }
-
-  response.writeHead(200, headers)
-  response.end(body)
-}
+const { errorHandler , returnRequestBody, sendResponseBodyToClient } = require('../utils/middleware')
 
 const cryptoToolAPI = async (crypto, fiat, request, response) => {
 
@@ -59,7 +22,6 @@ const cryptoToolAPI = async (crypto, fiat, request, response) => {
   } catch (err) {
     errorHandler(err, request, response)
   }
-
 
 }
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,8 +1,10 @@
 const logger = require('./logger')
 
 const unknownEndpoint = (request, response) => {
+
   response.statusCode = 404
   response.end()
+
 }
 
 const errorHandler = (err, request, response) => {
@@ -23,7 +25,46 @@ const errorHandler = (err, request, response) => {
 
 }
 
+const returnRequestBody = async (request) => {
+
+  let body = await new Promise(function (resolve, reject) {
+
+    let requestBody = []
+    request.on('data', (chunk) => {
+      requestBody.push(chunk)
+    })
+
+    request.on('end', () => {
+      try {
+        requestBody = JSON.parse(Buffer.concat(requestBody))
+      } catch (err) {
+        reject(err)
+      }
+      resolve(requestBody)
+    })
+  })
+
+  return body
+}
+
+const sendResponseBodyToClient = (body, response) => {
+
+  body = JSON.stringify(body)
+
+  const headers = {
+    'Content-Type': 'application/json;charset=utf-8',
+    'Content-Length': Buffer.byteLength(body),
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST'
+  }
+
+  response.writeHead(200, headers)
+  response.end(body)
+}
+
 module.exports = {
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  returnRequestBody,
+  sendResponseBodyToClient
 }
