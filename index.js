7523b383b1f56aa7b95634523ad0b30ad5c6eff4
Tommi <tmmi.makela@outlook.com>
2022-02-02 10:38:43 +0200

    docs: Add comments

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+/**
+ * Module dependencies
+ */
 const http = require('http')
 const api = require('./api/api')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
-// Create server
+/**
+ * Create http server application
+ */
 const server = http.createServer()
 
-// Listen to the request event
+// Listener to the request events
 server.on('request', (request, response) => {
   api(request, response) // Controller layer
 })
 
+// Listener to the specified port. Starts the HTTP server listening for connections.
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
