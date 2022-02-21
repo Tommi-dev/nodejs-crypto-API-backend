c8bafd85ebc538a2dd6cd42beaa7ee2e459f9f4e
Tommi <tmmi.makela@outlook.com>
2022-02-21 10:38:09 +0200

    chore: Simplify

--- services/highest-trading-volume.js
+++ services/highest-trading-volume.js
@@ -2,6 +2,7 @@ const returnDateAndHighestTradingVolume = data => {
 
   let tradingVolumes = data.total_volumes.map(volume => volume[1])
   let dateThatHaveHighestTradingVolume = data.total_volumes.find(datapoint => datapoint[1] === Math.max(...tradingVolumes))
+  
   const dateAndHighestTradingVolume = {
     date: dateThatHaveHighestTradingVolume[0],
     volume_in_fiat: dateThatHaveHighestTradingVolume[1]
--- services/longest-bearish-trend.js
+++ services/longest-bearish-trend.js
@@ -1,14 +1,20 @@
 const returnLongestBearishTrend = data => {
+
   let days = [0]
   let di = 0
+
   for (let i = 1; i < data.prices.length; i++) {
+  
     if (data.prices[i][1] < data.prices[i - 1][1]) {
       days[di] += 1
       continue
     }
+  
     di += 1
     days.push(0)
+  
   }
+
   return Math.max(...days)
 }
 
