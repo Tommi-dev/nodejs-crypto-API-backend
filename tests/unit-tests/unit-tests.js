42445699f61a5f2b1b9acf80bdca6fb3ac3a05bd
Tommi <tmmi.makela@outlook.com>
2022-02-21 10:13:28 +0200

    test: Remove unnecessary tests

--- tests/unit-tests/unit-tests.js
+++ tests/unit-tests/unit-tests.js
@@ -1,5 +1,3 @@
 const { validationsTest } = require('./services-test/validations-test')
-const { convertISO8601ToUNIXTest } = require('./models-test/convertISO8601ToUNIX-test.js')
 
 validationsTest()
-convertISO8601ToUNIXTest()
